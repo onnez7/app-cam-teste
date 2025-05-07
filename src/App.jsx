79196f1b2530291a88f4d022be858f170234a029
onnez7 <ronifaria2018@gmail.com>
2025-05-07 19:23:06 -0300

    Correçao da camera

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,9 @@ export function App() {
   return (
     <Router>
       <Routes>
-        {/* Página padrão redireciona para medição */}
-        <Route path="/" element={<Navigate to="/medir" replace />} />
 
         {/* Página de medição */}
-        <Route path="/medir" element={<MedicaoOptica />} />
+        <Route path="/" element={<MedicaoOptica />} />
 
         {/* Página de resultado */}
         <Route path="/resultado" element={<ResultadoMedicao />} />
